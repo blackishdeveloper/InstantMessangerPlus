5222fd638b4577ecb2275aca69f0fca0c4481f2d
Amila <sales@blackcarbonfeast.com>
2019-07-16 15:12:54 +0530

    Register System

--- app/src/main/java/com/playrocksapplication/instantmessangerplus/RegisterActivity.java
+++ app/src/main/java/com/playrocksapplication/instantmessangerplus/RegisterActivity.java
@@ -2,12 +2,43 @@ package com.playrocksapplication.instantmessangerplus;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.annotation.NonNull;
+import android.support.v7.app.AppCompatActivity;
+import android.text.TextUtils;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.ProgressBar;
+import android.widget.Toast;
+
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
 
 public class RegisterActivity extends AppCompatActivity {
+
+    private EditText inputEmail, inputPassword;
+    private Button btnSignIn, btnSignUp, btnResetPassword;
+    private ProgressBar progressBar;
+    private FirebaseAuth auth;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_register);
+
+        //Get Firebase auth instance
+        auth = FirebaseAuth.getInstance();
+
+        btnSignIn = (Button) findViewById(R.id.button4);
+        btnSignUp = (Button) findViewById(R.id.regBtn);
+        inputEmail = (EditText) findViewById(R.id.regEmail);
+        inputPassword = (EditText) findViewById(R.id.regPassword);
+        progressBar = (ProgressBar) findViewById(R.id.progressBar);
+
+
+
+
     }
 }
